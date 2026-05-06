3d2afb7ce330893c6e92764bc219cf887bdccaea
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2026-05-06 10:58:52 +0700

    remove conflict

--- src/main/java/com/chat/nab/service/UserService.java
+++ src/main/java/com/chat/nab/service/UserService.java
@@ -68,9 +68,4 @@ public class UserService {
             redisTemplate.delete(sessionKey);
         }
     }
-
-    // Logic giải phóng user
-    public void releaseUser(String userId) {
-        redisTemplate.delete(String.format(ONLINE_KEY, userId));
-    }
 }
